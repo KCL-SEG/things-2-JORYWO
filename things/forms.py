5eb78ec3cdc2a87aa5b56bc2304dc77ec236cd65
Joshua Wong <jorywo@gmail.com>
2022-10-16 00:02:07 +0100

    Typo mistake caused bug

--- things/forms.py
+++ things/forms.py
@@ -5,6 +5,6 @@ from django import forms
 
 class ThingForm(forms.Form):
     name = forms.CharField(label="Name")
-    description = forms.TextArea(label="Description")
+    description = forms.Textarea(label="Description")
     quantity = forms.NumberInput(label="")
 
