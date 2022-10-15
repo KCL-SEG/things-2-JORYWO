205dcde093f6d90a484b66aa172eace20325137b
Joshua Wong <jorywo@gmail.com>
2022-10-15 23:59:45 +0100

    Things Form solution

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+
+class ThingForm(forms.Form):
+    name = forms.CharField(label="Name")
+    description = forms.TextArea(label="Description")
+    quantity = forms.NumberInput(label="")
+
